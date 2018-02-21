a6c5a74d8d8eb995cc439f5d68fa5e8dd8b634b0
nprofilovic <nprofilovic@gmail.com>
2018-02-21 12:16:26 +0100

    Update 2.1

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,40 +1,140 @@
 'use strict';
 
 import React, {Component} from "react";
-import { View, TouchableOpacity, Image} from "react-native";
+import { View, TouchableOpacity, Image, Modal, StyleSheet,Button} from "react-native";
 import css from "../styles/product";
 import style from "../styles/style";
-import { Container, Header, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon, Right } from 'native-base';
+import { Container, Header, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon, Right,  } from 'native-base';
 
 
 export default class ProductItem extends Component {
-    
+  constructor(){
+    super();
+    this.state= {
+      modalVisible: false,
+     
+    }
+  }
+  
+  
+  openModal() {
+    this.setState({ modalVisible: true });
+  }
+
+  closeModal(){
+    this.setState({ modalVisible: false });
+  }
 	render() {
-       
+    const cards = [
+      {
+        text: 'Card One',
+        name: 'One',
+        image: require('../img/get-1.jpg'),
+      },
+      {
+        text: 'Card two',
+        name: 'One',
+        image: require('../img/get-1.jpg'),
+      },
+      {
+        text: 'Card tree',
+        name: 'One',
+        image: require('../img/get-1.jpg'),
+      },
+    ]  
 		return (
-            <Card style={{ elevation: 3 }}>
-                <CardItem onPress = {this.productCard}>
-                    <Left>
-                        <Thumbnail source={{uri: "http://www.bros-jeans.com/wp-content/uploads/2018/02/logobros-kocka.jpg"}} />
+            <View style={styles.container}>
+              
+              <Modal
+                  visible={this.state.modalVisible}
+                  animationType={'slide'}
+                  onRequestClose={() => this.closeModal()}
+              >
+                <View style={styles.modalContainer}>
+                  
+                <DeckSwiper
+                    dataSource={cards}
+                    renderItem={item =>
+                      <Card style={{ elevation: 3 }}>
+                        <CardItem>
+                          <Left>
+                            <Thumbnail source={item.image} />
                             <Body>
-                              <Text>{this.props.product.name}</Text>
-                              <Text note>{this.props.product.categories[0].name}</Text>
+                              <Text>{item.text}</Text>
+                              <Text note>NativeBase</Text>
                             </Body>
+                          </Left>
+                        </CardItem>
+                        <CardItem cardBody>
+                          <Image style={{ height: 300, flex: 1 }} source={item.image} />
+                        </CardItem>
+                        <CardItem>
+                          <Icon name="heart" style={{ color: '#ED4A6A' }} />
+                          <Text>{item.name}</Text>
+                        </CardItem>
+                      </Card>
+                    }
+                  />
+
+
+                  
+                    <Button
+                        onPress={() => this.closeModal()}
+                        title="Close modal"
+                    >
+                    </Button>
+                  </View>
+                
+              </Modal>
+              
+              <TouchableOpacity onPress={() => this.openModal()}>             
+              <Card style={{ elevation: 3 }}>
+                  <CardItem>
+                      <Left>
+                          <Thumbnail source={{uri: "http://www.bros-jeans.com/wp-content/uploads/2018/02/logobros-kocka.jpg"}}  />
+                              <Body >
+                                <Text>{this.props.product.name} </Text>
+                                <Text note>{this.props.product.categories[0].name}</Text>
+                              </Body>
+                      </Left>
+                  </CardItem>
+                  <CardItem cardBody>
+                      <Image style={{ height: 550, flex: 1 }} source={{uri: this.props.product.images[0].src}} onPress={() => this.openModal()} />
+                  </CardItem>
+                  <CardItem style={{height: 80}}>
+                    <Left>
+                      <Text>{this.props.product.name}</Text>
                     </Left>
-                </CardItem>
-                <CardItem cardBody>
-                    <Image style={{ height: 550, flex: 1 }} source={{uri: this.props.product.images[0].src}} />
-                </CardItem>
-                <CardItem style={{height: 80}}>
-                    <Text>{this.props.product.name}</Text>
-                    
+                    <Body>
+                    <Button
+                        onPress={() => this.openModal()}
+                        title="Open modal"
+                    >
+                      <Text>Otvori</Text>
+                    </Button>
+                    </Body>  
                     <Right>
-                    <Text style={ { marginLeft: 50}}>{this.props.product.price }din.</Text>
+                      <Text style={ { marginLeft: 50}}>{this.props.product.price }din.</Text>
                     </Right>
-                </CardItem>
-            </Card>
-            
+                  </CardItem>
+              </Card>
+              </TouchableOpacity>
+            </View>
 		);
 	}
 
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  modalContainer: {
+    flex: 1,
+    
+    backgroundColor: 'white',
+  },
+  innerContainer: {
+    alignItems: 'center',
+  },
+});
